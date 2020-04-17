6a4f7ecf50e0cc569c0bf8f3711e3d0baed7eff3
mrbryantguy <55728391+mrbryantguy@users.noreply.github.com>
2020-04-16 20:17:59 -0600

    Add files via upload

---
+++ Lab7.ino
@@ -0,0 +1,20 @@
+extern "C" {
+  void initAD();
+  byte readAD(int pin);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  initAD(); // call assembly init A/D routine
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  byte v;
+  v = readAD(2); // call assembly read sensor routine, A/D pin #2
+  Serial.print(" light = ");
+  Serial.print(v, HEX);
+  Serial.print("\n");
+  delay(1000); // delay one second
+}
